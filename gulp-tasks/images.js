b30315b2f1ac8a15f0a077059f402f099855e04d
Maryna Nakvas <110901176+MarynaNakvas@users.noreply.github.com>
2022-08-12 00:09:37 +0300

    Update images.js

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,7 +1,7 @@
-const gulp = require('gulp')
-const imagemin = require('gulp-imagemin')
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
 
-module.exports = function imageMinify() {
+export default function imageMinify() {
   return gulp.src('src/images/**/*.{gif,png,jpg,svg,webp}')
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
@@ -18,4 +18,4 @@ module.exports = function imageMinify() {
       })
     ]))
     .pipe(gulp.dest('build/images'))
-}
+};
